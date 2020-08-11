c3cce02d00c1a55610a3070d9009b9470dcc0220
Hamish Bassett <hbassett@enttec.com>
2020-08-11 18:13:24 +1000

    Add ability to parse CSV and emit processed objects
    .

---
+++ src/parseCSV.js
@@ -0,0 +1,21 @@
+const stream = require('stream');
+const csv = require('fast-csv');
+const EventEmitter = require('events');
+
+class CSVParser extends EventEmitter {
+  parseCSV(readStream) {
+    if (!(readStream instanceof stream.Readable)) {
+      throw new Error('readStream must be a ReadStream');
+    }
+    readStream
+      .setEncoding('utf-8')
+      .pipe(csv.parse({ headers: true }))
+      .on('error', (err) => {
+        this.emit('error', err);
+      })
+      .on('data', (data) => this.emit('data', data))
+      .on('close', () => this.emit('close'));
+  }
+}
+
+module.exports = CSVParser;
---
+++ test/testParseCSV.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const rewire = require('rewire');
+const fs = require('fs');
+const mock = require('mock-fs');
+
+const CSVParser = rewire('../src/parseCSV');
+
+const { expect } = chai;
+chai.should();
+
+const candidate = new CSVParser();
+
+describe('parseCSV', () => {
+  it('the module should export a function', () => {
+    expect(candidate.parseCSV).to.be.a('function');
+  });
+  it('the method should fail if the input is not a ReadStream', () => {
+    expect(() => {
+      candidate.parseCSV();
+    }).to.throw();
+  });
+  it('should throw an error if the stream errors', () => {
+    mock({
+      'foo.csv': mock.file({
+        content: 'foo, bar\n1, 2\n\n',
+      }),
+    });
+    const fileStream = fs.createReadStream('foo.csv');
+    candidate.parseCSV(fileStream);
+  });
+});
