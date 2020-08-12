b4094517f22681ddc31414047f7d76d209902848
Hamish Bassett <hbassett@enttec.com>
2020-08-12 17:42:10 +1000

    Add calls to task3 logic in main runtime
    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const getFileReader = require('./src/getFileStream');
 const ParseCSV = require('./src/parseCSV');
 const RegionAccumulator = require('./src/regionAccumulator');
 const OrderAccumulator = require('./src/orderAccumulator');
+const ShippingDateAccumulator = require('./src/shippingTimeAccumulator');
 const outputObject = require('./src/writeOutput');
 
 const filePath = 'target.csv';
@@ -10,15 +11,18 @@ const fileStream = getFileReader(filePath);
 const csvParser = new ParseCSV();
 const regionAccumulator = new RegionAccumulator();
 const orderAccumulator = new OrderAccumulator();
+const shippingTimeAccumulator = new ShippingDateAccumulator();
 
 csvParser.on('data', (data) => {
   regionAccumulator.parseData(data);
   orderAccumulator.parseData(data);
+  shippingTimeAccumulator.parseData(data);
 })
   .on('end', () => {
     regionAccumulator.convertNumbersToString();
     outputObject(regionAccumulator, 'task1.json');
     outputObject(orderAccumulator.years, 'task2.json');
+    outputObject(shippingTimeAccumulator.years, 'task3.json');
   });
 
 csvParser.parseCSV(fileStream);
