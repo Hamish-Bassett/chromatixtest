937474524602e1cdcee8021ad6c6bb40ebc3d7df
Hamish Bassett <hbassett@enttec.com>
2020-08-12 17:40:02 +1000

    Add some explanation of reasoning
    .

--- src/parseCSV.js
+++ src/parseCSV.js
@@ -3,10 +3,17 @@ const csv = require('fast-csv');
 const EventEmitter = require('events');
 
 class CSVParser extends EventEmitter {
+  /**
+   * by making the stream a variable we can inject what ever we need. We can easily plumb in a
+   * network sourced stream.
+   */
+  // network base
   parseCSV(readStream) {
     if (!(readStream instanceof stream.Readable)) {
       throw new Error('readStream must be a ReadStream');
     }
+    // we want to do minimal processing here and allow hooks for the calling closures to latch into
+    // so we can easily extend behaviour.
     readStream
       .setEncoding('utf-8')
       .pipe(csv.parse({ headers: true }))
